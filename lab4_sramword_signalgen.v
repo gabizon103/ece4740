1fc0f9554cda38317b9299a4bac4d54a64a26597
gabizon103 <ehg54@cornell.edu>
2024-05-17 13:43:28 -0400

    delete dylans dumb shit

--- lab4_sramword_signalgen.v
+++ lab4_sramword_signalgen.v
@@ -1,129 +1,146 @@
-// VerilogA signal generator for testing a single word of SRAM
+// VerilogA for ece4740, lab4_write_driver_sigGen, veriloga
 
 `include "constants.vams"
 `include "disciplines.vams"
 
-`define WORD_BITS 8
+// Use macro to define adder's bit width
+`define DATABITS 8
 
-module lab4_sramtop_signalgen(wordline, data_in, wen, clk);
+module lab4_write_driver_sigGen(WE, D_IN, word);
+// Declare outputs
+output WE;
+output [`DATABITS-1:0] D_IN;
+output word;
 
-// outputs from signalgen
 
-output [`WORD_BITS-1:0] data_in;
-output wordline;
-output wen;
-output clk;
+// Specify desired output type to be a continuous voltage signal
+voltage WE;
+voltage [`DATABITS-1:0] D_IN;
+voltage word;
 
-voltage [`WORD_BITS-1:0] data_in;
-voltage wordline;
-voltage wen;
-voltage clk;
 
+// Declare locally-scoped constants using localparam keyword
+// Clock period for test cases
 localparam cp = 4000p;
+// Clock half-period for FF clock latch signal
 localparam cp2 = 2000p;
 
-// number of tests
-localparam tb = 7;
+// Number of testcases per testsuite
+localparam tb1 = 2;
+localparam tb2 = 2;
+localparam tb3 = 2;
+localparam tb4 = 2;
+localparam tb5 = 2;
+localparam tb6 = 2;
 
-// clk constants
-real clk_sig;
-parameter real clk_period = cp from (0:inf);
-parameter real clk_period2 = cp2 from (0:inf);
-parameter real trise = 20p from [0:inf];
-parameter real tfall = 20p from [0:inf];
+// Tracks clock signal high/low value
+real			clk_sig;
 
-// voltage constants
-parameter real v_high = 1.2;
-parameter real v_low = 0.0;
+// Constants for clock periods
+parameter real	clk_period = cp from (0:inf);
+parameter real	clk_period2 = cp2 from (0:inf);
+parameter real	trise = 20p from [0:inf];
+parameter real	tfall = 20p from [0:inf];
 
-// integer variables for signal gen outputs
-integer data_in_cur;
-integer wordline_cur;
-integer wen_cur;
+// Constants for voltage signal bounds
+parameter real	v_high = 1.2;
+parameter real	v_low = 0.0;
 
-// track # of tests completed
-integer cur_test_count = 0;
+// Initialize integer variables for signal generator outputs
+// A, B, Cin are inputs to the N-bit adder
+integer 			WE_curr;
+integer        	D_IN_curr;
+integer			word_curr;
 
-// loop counter
-genvar j;
+// Keeps track of how many testcases have completed
+integer			curTestCount = 0;
 
-// analog desc
+// Initialize counters to be used in for-loop
+// Cannot use integers as the for-loop used is a "generator construct"
+genvar			j;
+
+
+// Start analog description of module
 analog begin
-    @(initial_step) begin
-        wordline_cur = 0;
-        data_in_cur = 8'd0;
-        wen_cur = 0;
-    end
 
-    // oscillate clk signal
-    @(timer(1n, clk_period2)) begin
-        if (clk_sig == v_low) clk_sig = v_high;
-        else clk_sig = v_low;
-    end
+	// Initialize signal generator output values
+	@(initial_step) begin
+		WE_curr = 0;
+		D_IN_curr = 0;
+		word_curr = 0;
+	end
 
-    // set clk voltage
-    V(clk) <+ transition(clk_sig, 0, trise, tfall);
+/*
+ 	-------------------------------------------------
+				TESTCASE DEFINITIONS
+	-------------------------------------------------
+*/
 
-    @(timer(0.5n, clk_period)) begin
-        // test 1
-        case (cur_test_count)
-        // test 0: write 10101010
-        0: begin
-            wen_cur = 1;
-            wordline_cur = 1;
-            data_in_cur = 8'b10101010; 
-        end
-        // test 1: do a read
-        1: begin
-            wen_cur = 0;
-            wordline_cur = 1;
-            data_in_cur = 0;
-        end
-        // test 2: write 01010101
-        2: begin
-            wen_cur = 1;
-            wordline_cur = 1;
-            data_in_cur = 8'b01010101;
-        end
-        // test 3: do a read
-        3: begin
-            wen_cur = 0;
-            wordline_cur = 1;
-            data_in_cur = 0;
-        end
-        // test 4: write 11111111
-        4: begin
-            wen_cur = 1;
-            wordline_cur = 1;
-            data_in_cur = 8'b11111111;
-        end
-        // test 5; do a read
-        5: begin
-            wen_cur = 0;
-            wordline_cur = 1;
-            data_in_cur = 0;
-        end
-        // test 6: write 00000000
-        6: begin
-            wen_cur = 1;
-            wordline_cur = 1;
-            data_in_cur = 8'b00000000;
-        end
-        // test 7: read
-        7: begin
-            wen_cur = 0;
-            wordline_cur = 1;
-            data_in_cur = 0;
-        end
-        endcase
-        cur_test_count = cur_test_count + 1;
-    end
+	// Timer block to run testcases
+	@(timer(0.5n,clk_period)) begin
 
-    for (j = 0; j < `WORD_BITS; j = j+1) begin
-        V(data_in[j]) <+ transition(data_in_cur & (1<<j) ? v_high : v_low, 0, trise, tfall);
-    end
-    V(wen) <+ transition(wen_cur ? v_high : v_low, 0, trise, tfall);
-    V(wordline) <+ transition(wordline_cur ? v_high : v_low, 0, trise, tfall);
+	// Testsuite 1: Write 1
+		curTestCount = curTestCount + 1;
+		if (curTestCount <= tb1) begin
+			WE_curr = 1;
+			D_IN_curr = 1;
+			if (curTestCount == tb1)
+				word_curr = 1;
+		end
+
+	// Testsuite 2: Read 1
+		if (tb1 < curTestCount && curTestCount <= tb1 + tb2) begin
+			word_curr = 0;
+			WE_curr = 0;
+			D_IN_curr = 0;
+			if (curTestCount == tb1 + tb2)
+				word_curr = 1;
+		end
+
+	// Testsuite 3: Write 0
+		if (tb1 + tb2 < curTestCount && curTestCount <= tb1 + tb2 + tb3) begin
+			word_curr = 0;
+			WE_curr = 1;
+			D_IN_curr = 0;
+			if (curTestCount == tb1 + tb2 + tb3)
+				word_curr = 1;
+		end
+
+	// Testsuite 4: Read 0
+		if (tb1 + tb2 + tb3 < curTestCount && curTestCount <= tb1 + tb2 + tb3 + tb4) begin
+			word_curr = 0;
+			WE_curr = 0;
+			D_IN_curr = 0;
+			if (curTestCount == tb1 + tb2 + tb3 + tb4)
+				word_curr = 1;
+		end
+
+	// Testsuite 5: Write 1
+		if (tb1 + tb2 + tb3 + tb4 < curTestCount && curTestCount <= tb1 + tb2 + tb3 + tb4 + tb5) begin
+			word_curr = 0;
+			WE_curr = 1;
+			D_IN_curr = 1;
+			if (curTestCount == tb1 + tb2 + tb3 + tb4 + tb5)
+				word_curr = 1;
+		end
+
+	// Testsuite 6: Read 1
+		if (tb1 + tb2 + tb3 + tb4 + tb5 < curTestCount && curTestCount <= tb1 + tb2 + tb3 + tb4 + tb5 + tb6) begin
+			WE_curr = 0;
+			D_IN_curr = 0;
+			word_curr = 0;
+			if (curTestCount == tb1 + tb2 + tb3 + tb4 + tb5 + tb6)
+				word_curr = 1;
+		end
+	end
+
+
+	// Parse integer testcase inputs into binary,
+	// write resultant binary to N-bit output pins
+	V(WE) <+ transition(WE_curr ? v_high : v_low, 0, trise, tfall);
+	V(D_IN) <+ transition(D_IN_curr ? v_high : v_low, 0, trise, tfall);
+	V(word) <+ transition(word_curr ? v_high : v_low, 0, trise, tfall);
+
 end
 
 endmodule
